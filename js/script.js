218f1dd0eca96f755fc4fdb142b14cb76ddcb6c7
Wenard Santos <wenardken57@gmail.com>
2025-04-20 17:21:05 +0800

    add book form and declare style of books

--- js/script.js
+++ js/script.js
@@ -1,23 +1,35 @@
 const myLibrary = [];
 
-const bookTitleInput = document.querySelector("#book-title");
-const bookAuthorInput = document.querySelector("#book-author");
-const addBookBtn = document.querySelector("#add-book-btn");
+const newBookForm = document.querySelector("#new-book-form");
 
-function Book(id, title, author) {
+const bookDisplayContainer = document.createElement("div");
+
+function Book(id, title, author, pages, isRead) {
   if (!new.target) {
     throw Error("Use new keyword to call Book constructor");
   }
   this.id = id;
   this.title = title;
   this.author = author;
+  this.pages = pages;
+  this.isRead = isRead;
 }
 
-function addBookToTheLibrary(title, author) {
-  const book = new Book(crypto.randomUUID(), title, author);
+function addBookToTheLibrary(title, author, pages, isRead) {
+  const book = new Book(crypto.randomUUID(), title, author, pages, isRead);
   myLibrary.push(book);
 }
 
-addBookBtn.addEventListener("click", () => {
-  addBookToTheLibrary(bookTitleInput.value, bookAuthorInput.value);
-});
+newBookForm.addEventListener('submit', (e) => {
+  e.preventDefault(); // No reloads
+
+  addBookToTheLibrary(
+    newBookForm.elements['title'].value,
+    newBookForm.elements['author'].value,
+    newBookForm.elements['pages'].value,
+    newBookForm.elements['read'].checked
+  );
+  
+  // hide form
+  newBookForm.setAttribute('style', "display: none;");
+})
