6c046da10b77e6137bdca44a32adb5529d2584ef
Wenard Santos <wenardken57@gmail.com>
2025-04-20 16:34:25 +0800

    add take title, author from user input

--- README.md
+++
@@ -1,9 +0,0 @@
-# Basic starter for HTML, CSS, and JavaScript projects
-
-Don't want to waste my time doing the same set up everytime I start a new project
-
-
-# Includes
-
-- John Cameau CSS reset
-- HTML structure
--- js/script.js
+++ js/script.js
@@ -1,3 +1,23 @@
-document.addEventListener("DOMContentLoaded", () => {
-  console.log("Script Loaded!");
-})
+const myLibrary = [];
+
+const bookTitleInput = document.querySelector("#book-title");
+const bookAuthorInput = document.querySelector("#book-author");
+const addBookBtn = document.querySelector("#add-book-btn");
+
+function Book(id, title, author) {
+  if (!new.target) {
+    throw Error("Use new keyword to call Book constructor");
+  }
+  this.id = id;
+  this.title = title;
+  this.author = author;
+}
+
+function addBookToTheLibrary(title, author) {
+  const book = new Book(crypto.randomUUID(), title, author);
+  console.log(`${book.title} ${book.author}`);
+}
+
+addBookBtn.addEventListener("click", () => {
+  addBookToTheLibrary(bookTitleInput.value, bookAuthorInput.value);
+});
