12141e92ca3cd3b08e2ebd31a0b92a8b55b15e96
Wenard Santos <wenardken57@gmail.com>
2025-04-20 17:56:42 +0800

    add display book upon submission

--- js/script.js
+++ js/script.js
@@ -2,9 +2,14 @@ const myLibrary = [];
 
 const newBookForm = document.querySelector("#new-book-form");
 
-const bookDisplayContainer = document.createElement("div");
+const libraryContainer = document.querySelector(".library-container");
 
-function Book(id, title, author, pages, isRead) {
+const bookDisplay = document.createElement('div');
+
+bookDisplay.classList.add('book');
+
+
+function Book(id, title, author, pages, read) {
   if (!new.target) {
     throw Error("Use new keyword to call Book constructor");
   }
@@ -12,24 +17,32 @@ function Book(id, title, author, pages, isRead) {
   this.title = title;
   this.author = author;
   this.pages = pages;
-  this.isRead = isRead;
+  this.read = read;
 }
 
-function addBookToTheLibrary(title, author, pages, isRead) {
-  const book = new Book(crypto.randomUUID(), title, author, pages, isRead);
+function addBookToTheLibrary(title, author, pages, read) {
+  const book = new Book(crypto.randomUUID(), title, author, pages, read);
   myLibrary.push(book);
+  return book;
+}
+
+function displayBook(book) {
+  const newBook = bookDisplay.cloneNode(true);
+  newBook.textContent = `${book.title} by ${book.author}, ${book.pages} pages, ${book.read}`;
+  libraryContainer.appendChild(newBook);
 }
 
 newBookForm.addEventListener('submit', (e) => {
   e.preventDefault(); // No reloads
 
-  addBookToTheLibrary(
+  displayBook(addBookToTheLibrary(
     newBookForm.elements['title'].value,
     newBookForm.elements['author'].value,
     newBookForm.elements['pages'].value,
     newBookForm.elements['read'].checked
-  );
+  ));
   
   // hide form
   newBookForm.setAttribute('style', "display: none;");
-})
+});
+
